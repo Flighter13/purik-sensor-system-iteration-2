37fafaf215d535e60d537b44bafae31ba1e1360f
Joshua Parr <131053642+Flighter13@users.noreply.github.com>
2026-09-13 00:45:33 -0400

    Implement GY521 measurement driver interface

--- sensor-node-r4/drivers/GY521Driver.h
+++ sensor-node-r4/drivers/GY521Driver.h
@@ -4,6 +4,17 @@
 #include <Wire.h>
 #include "../core/SensorDriver.h"
 
+struct GY521Reading {
+  float accelXMps2;
+  float accelYMps2;
+  float accelZMps2;
+  float gyroXDps;
+  float gyroYDps;
+  float gyroZDps;
+  float temperatureC;
+  uint32_t timestampMs;
+};
+
 class GY521Driver : public SensorDriver {
  public:
   GY521Driver(TwoWire& wire, uint8_t address, uint16_t sensorId);
@@ -21,9 +32,14 @@ class GY521Driver : public SensorDriver {
   const char* name() const override;
   const Purik::SensorDescriptor& descriptor() const override;
 
+  const GY521Reading& reading() const;
+  bool hasFreshReading() const;
+  void clearFreshReading();
+
  private:
   bool readRegister(uint8_t reg, uint8_t& value);
   bool writeRegister(uint8_t reg, uint8_t value);
+  bool readBurst(uint8_t startReg, uint8_t* buffer, size_t length);
 
   TwoWire& wire_;
   uint8_t address_;
@@ -31,6 +47,8 @@ class GY521Driver : public SensorDriver {
   bool present_;
   bool initialized_;
   bool healthy_;
+  bool freshReading_;
   uint32_t lastUpdateMs_;
+  GY521Reading reading_;
   Purik::SensorDescriptor descriptor_;
 };
