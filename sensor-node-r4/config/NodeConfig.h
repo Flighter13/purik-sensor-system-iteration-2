8858e90834333e7eac363de5701939b3fccb9a43
Joshua Parr <131053642+Flighter13@users.noreply.github.com>
2026-09-13 00:10:22 -0400

    Add sensor-node feature switches

---
+++ sensor-node-r4/config/NodeConfig.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace PurikNode {
+
+constexpr const char* NODE_NAME = "sensor-node-01";
+
+constexpr bool ENABLE_RADAR = false;
+constexpr bool ENABLE_LIDAR = false;
+constexpr bool ENABLE_GNSS = true;
+constexpr bool ENABLE_IMU = true;
+constexpr bool ENABLE_NRF24 = false;
+
+}  // namespace PurikNode
