4a994308d92fbc2efa6bdb5ddbff9c9debdc54c2
Joshua Parr <131053642+Flighter13@users.noreply.github.com>
2026-09-13 00:09:50 -0400

    Add Purik protocol version header

---
+++ common/PurikVersion.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Arduino.h>
+
+namespace Purik {
+
+constexpr uint8_t PROTOCOL_VERSION_MAJOR = 1;
+constexpr uint8_t PROTOCOL_VERSION_MINOR = 0;
+constexpr uint8_t PROTOCOL_VERSION_PATCH = 0;
+
+}  // namespace Purik
