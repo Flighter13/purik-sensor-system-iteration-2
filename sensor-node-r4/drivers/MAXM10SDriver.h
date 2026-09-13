7ef8e282d4d8678dfdb4ec3164d63f09c5bd7c8d
Joshua Parr <131053642+Flighter13@users.noreply.github.com>
2026-09-13 00:45:07 -0400

    Implement MAX-M10S measurement driver interface

--- sensor-node-r4/drivers/MAXM10SDriver.h
+++ sensor-node-r4/drivers/MAXM10SDriver.h
@@ -2,7 +2,20 @@
 
 #include <Arduino.h>
 #include <Wire.h>
+#include <SparkFun_u-blox_GNSS_v3.h>
 #include "../core/SensorDriver.h"
+
+struct MAXM10SReading {
+  int32_t latitudeE7 = 0;
+  int32_t longitudeE7 = 0;
+  int32_t altitudeMm = 0;
+  uint32_t groundSpeedMmS = 0;
+  uint32_t headingE5 = 0;
+  uint8_t satellites = 0;
+  uint8_t fixType = 0;
+  bool valid = false;
+  uint32_t timestampMs = 0;
+};
 
 class MAXM10SDriver : public SensorDriver {
  public:
@@ -21,6 +34,10 @@ class MAXM10SDriver : public SensorDriver {
   const char* name() const override;
   const Purik::SensorDescriptor& descriptor() const override;
 
+  const MAXM10SReading& reading() const;
+  bool hasFreshReading() const;
+  void clearFreshReading();
+
  private:
   TwoWire& wire_;
   uint8_t address_;
@@ -28,6 +45,9 @@ class MAXM10SDriver : public SensorDriver {
   bool present_;
   bool initialized_;
   bool healthy_;
+  bool freshReading_;
   uint32_t lastUpdateMs_;
+  SFE_UBLOX_GNSS gnss_;
+  MAXM10SReading reading_;
   Purik::SensorDescriptor descriptor_;
 };
