024c3c8e01c1fbf9c03a013989f1450df7dbd844
Joshua Parr <131053642+Flighter13@users.noreply.github.com>
2026-09-13 00:18:06 -0400

    Extend sensor driver interface for registry discovery

--- sensor-node-r4/core/SensorDriver.h
+++ sensor-node-r4/core/SensorDriver.h
@@ -2,6 +2,7 @@
 
 #include <Arduino.h>
 #include "../../common/PurikSensorTypes.h"
+#include "../../common/SensorDescriptor.h"
 
 class SensorDriver {
  public:
@@ -12,7 +13,12 @@ class SensorDriver {
   virtual void update() = 0;
   virtual bool healthy() const = 0;
 
+  virtual bool present() const = 0;
+  virtual bool initialized() const = 0;
+  virtual uint32_t lastUpdateMs() const = 0;
+
   virtual uint16_t sensorId() const = 0;
   virtual Purik::SensorClass sensorClass() const = 0;
   virtual const char* name() const = 0;
+  virtual const Purik::SensorDescriptor& descriptor() const = 0;
 };
